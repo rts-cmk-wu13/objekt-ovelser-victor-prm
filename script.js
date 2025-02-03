e8624e2943664d9e424b26b752534e1a25168648
victor-prm <victorpabb@gmail.com>
2025-02-03 10:40:21 +0100

    solved task 1

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("We're live");
+let soccerPlayer = {
+    firstName: "Michael",
+    lastName: "Laudrup",
+    age: 60,
+    clubs: ["Ajax", "Vissel Kobe", "Real Madrid", "Barcelona", "Juventus", "Lazio", "Brøndby", "KB"],
+    birthday: new Date("1960-06-15"),
+    ['Is Active']: false,
+}
+
+console.log(soccerPlayer.firstName, soccerPlayer.lastName, soccerPlayer['clubs'], soccerPlayer['birthday'],soccerPlayer['Is Active']);
