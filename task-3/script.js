e0f315f86a6c3f5a229cec4f0a8b0522bf09ca64
victor-prm <victorpabb@gmail.com>
2025-02-03 11:33:30 +0100

    solved task 2 and and its extras

--- task-3/script.js
+++ task-3/script.js
@@ -4,5 +4,14 @@ let students = [
     {name: "Emil", age: 28, course: "Teknisk Designer"},
 ]
 
-let studentListContainer = document.querySelector('.student-list');
-console.log(studentListContainer);
+let studentListContainer = document.querySelector('.student-list'); 
+students.forEach(function(student){
+    const studentDiv = document.createElement('div');
+    studentDiv.classList.add('student');
+    let studentHeadline = document.createElement('h2');
+    studentHeadline.textContent = student.name;
+    let studentInfo = document.createElement('p');
+    studentInfo.textContent = `Alder: ${student.age}, Uddannelse: ${student.course}`;
+    studentDiv.append(studentHeadline, studentInfo)
+    studentListContainer.append(studentDiv);
+})
